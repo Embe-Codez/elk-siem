7b192c5e723a67520fbd5ea6e4ae9a55ec2bb512
marcos blount <marcos96blount@gmail.com>
2023-05-27 10:18:38 -0500

    Add error handling to dashboard code

--- dashboard/main.py
+++ dashboard/main.py
@@ -7,62 +7,58 @@ from data import queries
 
 class KibanaDashboard:
     def __init__(self):
-        self.client  = Elasticsearch(hosts=[config.ELASTICSEARCH_HOST])
-        self.data    = self.get_data()
-        self.df      = self.clean_data(self.data)
-        self.layout  = go.Layout(title='My Home Network Traffic Dashboard')
+        self.client = Elasticsearch(hosts=[config.ELASTICSEARCH_HOST])
+        self.data = self.get_data()
+        self.df = self.clean_data(self.data)
+        self.layout = go.Layout(title='My Home Network Traffic Dashboard')
         self.figures = self.create_visualizations()
 
     def get_data(self):
         data = []
         try:
             for index in range(config.DAYS_TO_QUERY):
-                res   = self.client.search(index='network-traffic-*', body=queries.traffic_query(index))
+                res = self.client.search(index='network-traffic-*', body=queries.traffic_query(index))
                 data += res['hits']['hits']
         except Exception as e:
             print(f"Error occurred while fetching data from Elasticsearch: {str(e)}")
         return data
 
     def clean_data(self, data):
-        df = pd.json_normalize(data)
-        df = df[[
-            '@timestamp',
-            'source.ip',
-            'source.port',
-            'destination.ip',
-            'destination.port',
-            'event.duration',
-            'event.action'
-        ]]
-        df['@timestamp']        = pd.to_datetime(df['@timestamp'], utc=True)
-        df['event.duration']    = pd.to_timedelta(df['event.duration'])
-        df['event.duration_ms'] = df['event.duration'] / pd.Timedelta(milliseconds=1)
-        df['hour'] = df['@timestamp'].dt.hour
-        df['day']  = df['@timestamp'].dt.date
-        df['protocol'] = np.where(df['destination.port'] == 80, 'HTTP', 'HTTPS')
-        return df
+        try:
+            df = pd.json_normalize(data)
+            df = df[['@timestamp', 'source.ip', 'source.port', 'destination.ip', 'destination.port', 'event.duration', 'event.action']]
+            df['@timestamp'] = pd.to_datetime(df['@timestamp'], utc=True)
+            df['event.duration'] = pd.to_timedelta(df['event.duration'])
+            df['event.duration_ms'] = df['event.duration'] / pd.Timedelta(milliseconds=1)
+            df['hour'] = df['@timestamp'].dt.hour
+            df['day'] = df['@timestamp'].dt.date
+            df['protocol'] = np.where(df['destination.port'] == 80, 'HTTP', 'HTTPS')
+            return df
+        except (pd.JSONDecodeError, KeyError, ValueError) as e:
+            raise DataParsingError("Error occurred during data parsing") from e
 
     def create_visualizations(self):
-        fig1 = go.Figure()
-        fig1.add_trace(go.Scatter(x=self.df['@timestamp'], y=self.df['event.duration_ms'], mode='markers',
-                                  name='Response Time'))
+        try:
+            fig1 = go.Figure()
+            fig1.add_trace(go.Scatter(x=self.df['@timestamp'], y=self.df['event.duration_ms'], mode='markers', name='Response Time'))
 
-        fig2 = go.Figure()
-        fig2.add_trace(go.Bar(x=self.df['day'], y=self.df['event.duration_ms'], name='Response Time'))
+            fig2 = go.Figure()
+            fig2.add_trace(go.Bar(x=self.df['day'], y=self.df['event.duration_ms'], name='Response Time'))
 
-        fig3 = go.Figure()
-        fig3.add_trace(go.Bar(x=self.df['hour'], y=self.df['event.duration_ms'], name='Response Time'))
+            fig3 = go.Figure()
+            fig3.add_trace(go.Bar(x=self.df['hour'], y=self.df['event.duration_ms'], name='Response Time'))
 
-        fig4 = go.Figure()
-        fig4.add_trace(go.Pie(values=self.df['protocol'].value_counts(), labels=self.df['protocol'].unique(),
-                              name='Protocol'))
+            fig4 = go.Figure()
+            fig4.add_trace(go.Pie(values=self.df['protocol'].value_counts(), labels=self.df['protocol'].unique(), name='Protocol'))
 
-        figs = [fig1, fig2, fig3, fig4]
+            figs = [fig1, fig2, fig3, fig4]
 
-        for fig in figs:
-            fig.update_layout(self.layout)
+            for fig in figs:
+                fig.update_layout(self.layout)
 
-        return figs
+            return figs
+        except (ValueError, KeyError) as e:
+            raise VisualizationError("Error occurred during visualization creation") from e
 
 class Dashboard(KibanaDashboard):
     def display_visualizations(self):
